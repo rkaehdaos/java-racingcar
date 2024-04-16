e01f81bc26d1dd569de766c9676bd10ddd7ac840
Kai Ahn <kai.ahn@businesson.co.kr>
2024-04-16 21:46:16 +0900

    가독성

--- src/main/java/step4_winner/Race.java
+++ src/main/java/step4_winner/Race.java
@@ -8,12 +8,10 @@ import java.util.Random;
 
 public class Race {
     private final Random random = new Random();
-    @Getter
-    private final List<Car> cars;
-    @Getter
-    private final int tries;
+    @Getter private final List<Car> cars;
+    @Getter private final int tries;
 
-    public Race(List<Car> cars , int tries) {
+    public Race(List<Car> cars, int tries) {
         if (cars.isEmpty())
             throw new IllegalArgumentException("0이상이어야 함");
         this.cars = cars;
