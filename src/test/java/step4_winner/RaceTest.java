c7a7fdd87af51539789ef60de83cface4f5b73c2
Kai Ahn <kai.ahn@businesson.co.kr>
2024-04-16 21:36:42 +0900

    최적화

--- src/test/java/step4_winner/RaceTest.java
+++ src/test/java/step4_winner/RaceTest.java
@@ -1,7 +1,6 @@
 package step4_winner;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,7 +8,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 
 class RaceTest {
